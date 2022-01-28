6ba89506d7d0afbba912c9d303ca2c7dda4a3001
Michael Lobman <michael.lobman@gmail.com>
2022-01-28 11:35:40 -0500

    Correct Distillery file extension

--- src/components/Region.js
+++ src/components/Region.js
@@ -1,27 +1,45 @@
 import React from "react"
+import Distillery from "./Distillery.js"
 
 function Region({ name, distilleries }){
 
-    const distilleryList = distilleries.map(distillery => {
-
-        const bottleListItems = distillery.bottles.map(bottle => {
-            return(
-                <li key={bottle.id}>{bottle.name}</li>
-            )
-        })
-
+    const distilleryItems = distilleries.map(distillery => {
         return (
-        <div key={distillery.id}>
-            <h3>{distillery.name}</h3>
-            <ul>
-                {bottleListItems}
-            </ul>
-        </div>
-    )})
-    return(
+            <Distillery 
+                key={distillery.id} 
+                name={distillery.name}
+                bottles={distillery.bottles}
+            />
+        )
+    })
+
+    // const distilleryList = distilleries.map(distillery => {
+
+    //     const bottleListItems = distillery.bottles.map(bottle => {
+    //         return(
+    //             <li key={bottle.id}>{bottle.name}</li>
+    //         )
+    //     })
+
+    //     return (
+    //     <div key={distillery.id}>
+    //         <h3>{distillery.name}</h3>
+    //         <ul>
+    //             {bottleListItems}
+    //         </ul>
+    //     </div>
+    // )})
+    // return(
+    //     <div>
+    //         <h2>{name}</h2>
+    //         {distilleryList}
+    //     </div>
+    // )
+
+    return (
         <div>
             <h2>{name}</h2>
-            {distilleryList}
+            {distilleryItems}
         </div>
     )
 }
